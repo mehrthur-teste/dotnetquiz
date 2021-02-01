fb86af5c1784cf6d33c33bac584dafba219172df
Mehrthur Silva <mehrthurfordevtools@gmail.com>
2021-02-01 01:07:15 -0300

    eliminou-se o erro de nomeclatura de pasta

--- src/components/BackLinkArrow/index.js
+++ src/components/BackLinkArrow/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import Link from '../Link';
+import Link from '../link';
 
 const StyledLink = styled(Link)`
   transition: .3s;
@@ -15,15 +15,15 @@ const SVG = styled.svg`
 `;
 
 export default function BackLinkArrow({ href }) {
-    return (
-        <StyledLink href={href} style={{ width: '24px', height: '24px', display: 'inline-block' }}>
-            <SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                <path fillRule="evenodd" clipRule="evenodd" d="M15.41 7.41L14 6L8 12L14 18L15.41 16.59L10.83 12L15.41 7.41Z" fill="white" fillOpacity="0.87" />
-            </SVG>
-        </StyledLink>
-    );
+  return (
+    <StyledLink href={href} style={{ width: '24px', height: '24px', display: 'inline-block' }}>
+      <SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+        <path fillRule="evenodd" clipRule="evenodd" d="M15.41 7.41L14 6L8 12L14 18L15.41 16.59L10.83 12L15.41 7.41Z" fill="white" fillOpacity="0.87" />
+      </SVG>
+    </StyledLink>
+  );
 }
 
 BackLinkArrow.propTypes = {
-    href: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
 };
